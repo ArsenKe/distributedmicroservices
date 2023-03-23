6f5bd5a46453caa6045a723de2e258edc22554f3
Arsen Keshishyan <a01576524@unet.univie.ac.at>
2023-03-23 19:44:39 +0100

    pom file config

--- customer/src/main/java/com/microservicesPetProject/customer/CustomerService.java
+++ customer/src/main/java/com/microservicesPetProject/customer/CustomerService.java
@@ -1,15 +1,20 @@
 package com.microservicesPetProject.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .name(request.name())
         .lastname(request.lastName())
         .email(request.email())
         .build();
+
+        customerRepository.save(customer);
 
     }
 
