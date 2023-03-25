1e2610303bbc83980e110a228df4b63f6e4ff7fc
Arsen Keshishyan <a01576524@unet.univie.ac.at>
2023-03-25 22:28:18 +0100

    fraudMicroService

---
+++ fraud/src/main/java/com/microservicePetProject/fraud/FraudApplication.java
@@ -0,0 +1,11 @@
+package com.microservicePetProject.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(FraudApplication.class, args);
+    }
+}
---
+++ fraud/src/main/java/com/microservicePetProject/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.microservicePetProject.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
